91cbdf9fff18baf61a7295f76fbe675807e09b38
Jamie Reilly <33383970+reilly-lab@users.noreply.github.com>
2025-04-29 14:08:24 +0100

    added new data

--- R/data.R
+++ R/data.R
@@ -1,59 +1,89 @@
-#' A Sample Dialogue Transcript
+#' Dialogue 'Dirty' Transcript
 #'
-#' Messy string data composed of missing observations, fragments, punctuations interspersed with single words. Two people talking to each other.
+#' A sample dyadic conversation transcript where two people are taslking; Messy string data in a conversation tramscript, mulyiple lines per person per turn, missing observations, fragments, punctuations interspersed with single words. Two people talking to each other.
 #'
-#'
-#' @format ## "DialogueSample1"
+#' @format ## "Dialogue_Dirty"
 #' A data frame with 75 rows and 2 columns:
 #' \describe{
 #'   \item{word}{text from a language transcript}
 #'   \item{speaker}{Mary or Peter: fictional speaker identities}
 #'   ...
 #' }
-"DialogueSample1"
+"Dialogue_Dirty"
+
+
+
+#' Dialogue Structured/Ideal Formatted Transcript
+#'
+#' Perfectly pre-fomrmatted data in a structure where 2 people are coversing in 1-word utterances back and forth.
+#'
+#' @format ## "Dialogue_Structured"
+#' A data frame with 50 rows and 2 vars:
+#' \describe{
+#'   \item{mytext}{text from a language transcript}
+#'   \item{speaker}{P1 or P2 fictional interlocutor identities}
+#'   ...
+#' }
+"Dialogue_Structured"
 
 
 
 
-#' A Sample Monologue Transcript
+#' A Sample 'Dirty' Monologue Transcript
 #'
 #' No talker delineated. Messy string data composed of missing observations, fragments, punctuations interspersed with single words.
 #'
 #'
-#' @format ## "MonologueSample1"
-#' A data frame with 74 rows and 1 column:
+#' @format ## "Monologue_Dirty"
+#' A data.frame with 74 obs and 1 var
 #' \describe{
-#'   \item{word}{text from a hypothetical language transcript}
+#'   \item{mytext}{text from a hypothetical language transcript}
 #'   ...
 #' }
-"MonologueSample1"
+"Monologue_Dirty"
+
+
+
+#' A Sample Structured Monologue Transcript
+#'
+#' No talker delineated. Idealized/structured transcript no missing observations, fragments, no multiword utterances, already split to one-word-per-row
+#'
+#' @format ## "Monologue_Structured"
+#' A data.frame with 25 obs, 2 vars:
+#' \describe{
+#'   \item{mytext}{text from a hypothetical 'ideal' language transcript}
+#'    \item{timestamp}{simulated metadata as a timestamp}
+#'   ...
+#' }
+"Monologue_Structured"
 
 
 
 
-#' Sample Transcript Split by Two Columns
+
+#' Sample Transcript Split by Two Columns for Pairwise Distance
 #'
 #' No talker delineated. User arrays first word in one column, second word in another column of the dataframe.
 #'
 #'
-#' @format ## "ColumnSample"
+#' @format ## "WordList_Columns"
 #' A data frame with 27 rows and 2 columns:
 #' \describe{
 #'   \item{word1}{text corresponding to the first word in a pair to contrast}
 #'   \item{word2}{text corresponding to the second word in a pair to contrast}
 #'   ...
 #' }
-"ColumnSample"
+"WordList_Columns"
 
 
 
 
-#' Sample Dataframe with Blocks of Words Related by Semantic Category
+#' Sample Dataframe with Blocks of Words Related by Semantic Category (unordered text) for evaluating hierachical clustering
 #'
 #' No talker delineated. Vector of 50 words, 10 from each of 5 categories (animals, fruits, weapons..).
 #'
 #'
-#' @format ## "FakeCats"
+#' @format ## "WordList_TestClustering"
 #' A data frame with 50 rows and 4 columns:
 #' \describe{
 #'   \item{ID_JR}{a sequential numeric identifier}
@@ -62,5 +92,5 @@
 #'   \item{prediction}{is the target word within category or at a switchpoint between cats}
 #'   ...
 #' }
-"FakeCats"
+WordList_TestClustering
 
